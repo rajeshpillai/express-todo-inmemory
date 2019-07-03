7e51d8c29ab86a3f31dd6dbf5db85aa00346f3de
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-07-03 18:13:07 +0530

    Moved data to data folder

--- api/todos.js
+++ api/todos.js
@@ -1,16 +1,8 @@
 var express = require('express')
 const bodyParser = require("body-parser");
-
+const db = require('../data/todos');
 var app = express.Router()
 
-
-var db = {
-  newTask: "",
-  tasks: [
-    { id: "1", title: "Task 1", done: "false", edit: "false" },
-    { id: "2", title: "Task 2", done: "true", edit: "false" }
-  ]
-};
 
 
 // middleware that is specific to this router
