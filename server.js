3c4be6b0c44eca62390fccee48467a7dc266b912
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-03-11 11:33:33 +0530

    Package: Install cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const cors = require('cors');
 
 app.set("view engine", "ejs");
 
+app.use(cors());
 app.use(express.static("public"));
 
 var port = 8888;
