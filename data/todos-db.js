e96e48a75dc35778bd1b53b3604a7e072e076979
Urvashi Amit Sachdev <urvashi.sachdev@algorisys.com>
2021-12-03 22:23:15 +0530

    Added createdby in data

--- data/todos-db.js
+++ data/todos-db.js
@@ -1,13 +1,25 @@
 var db = {
   newTask: "",
   tasks: [
-    { id: "1", title: "Task 1", done: "false", edit: "false" },
-    { id: "2", title: "Task 2", done: "true", edit: "false" }
+    {
+      id: "1",
+      title: "Task 1",
+      done: "false",
+      edit: "false",
+      createdby: "default",
+    },
+    {
+      id: "2",
+      title: "Task 2",
+      done: "true",
+      edit: "false",
+      createdby: "default",
+    },
   ],
   subtasks: [
     { id: "1", task_id: 1, title: "Sub Task 1", done: "false", edit: "false" },
     { id: "2", task_id: 1, title: "Sub Task 2", done: "false", edit: "true" },
-  ]
+  ],
 };
 
 module.exports = db;
